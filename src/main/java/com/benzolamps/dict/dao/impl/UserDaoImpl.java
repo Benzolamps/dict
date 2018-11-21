6aac867b81c34e6f54372aeb78d1b4e921a11351
Benzolamps <gwy@163.com>
2018-11-21 16:10:03 +0800

    小问题

--- src/main/java/com/benzolamps/dict/dao/impl/UserDaoImpl.java
+++ src/main/java/com/benzolamps/dict/dao/impl/UserDaoImpl.java
@@ -5,7 +5,7 @@ import com.benzolamps.dict.dao.base.UserDao;
 import org.springframework.stereotype.Repository;
 
 /**
- * 用户Dao皆苦实现类
+ * 用户Dao接口实现类
  * @author Benzolamps
  * @version 2.1.1
  * @datetime 2018-8-30 20:49:58
